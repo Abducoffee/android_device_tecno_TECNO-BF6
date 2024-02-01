35c395e7da2c56099217028cc4e2e9dd27a02b31
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2024-02-01 21:19:17 +0300

    vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-BF6-user
-add_lunch_combo omni_TECNO-BF6-userdebug
-add_lunch_combo omni_TECNO-BF6-eng
+add_lunch_combo twrp_TECNO-BF6-user
+add_lunch_combo twrp_TECNO-BF6-userdebug
+add_lunch_combo twrp_TECNO-BF6-eng
