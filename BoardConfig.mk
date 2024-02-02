afa7da877d7d7c7a92425658d89f7171bca299fb
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2024-02-02 13:41:26 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,11 @@
 DEVICE_PATH := device/tecno/TECNO-BF6
 
 # For building with minimal manifest
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
+# For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
 # A/B
